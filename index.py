5768c9183ab4d3e90362e4a6eb71b6566380672d
DecoMinerals <mja@decominerals.ru>
2025-04-10 14:37:41 +0300

    лучшею надо енв загрузить

--- index.py
+++ index.py
@@ -22,8 +22,8 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')  # Загружаем токен и
 CHAT_ID = os.getenv('CHAT_ID')  # Загружаем chat_id из .env
 
 # Email настройки
-SMTP_SERVER = 'smtp.gmail.com'
-SMTP_PORT = 587
+SMTP_SERVER = os.getenv('SMTP_SERVER')
+SMTP_PORT = os.getenv('SMTP_PORT')
 SENDER_EMAIL = os.getenv('SENDER_EMAIL')  # Загружаем email отправителя
 SENDER_PASSWORD = os.getenv('SENDER_PASSWORD')  # Загружаем пароль из .env
 RECEIVER_EMAIL = os.getenv('RECEIVER_EMAIL')  # Загружаем email получателя
