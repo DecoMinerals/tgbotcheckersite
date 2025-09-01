80201c4b4aa7720b8b6383e4154f39546151bccf
DecoMinerals <mja@decominerals.ru>
2025-09-01 13:39:16 +0300

    fix

--- index.py
+++ index.py
@@ -37,7 +37,7 @@ PASSWORD = os.getenv('PASSBOT')
 
 # --- Список сайтов ---
 SITES = [
-    "http://sdasdads.ru".
+    "http://sdasdads.ru",
     "https://silmer.org",
     "https://decomachinery.ru",
     "https://decominerals.ru",
